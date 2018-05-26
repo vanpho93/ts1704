6876b4056578dcd44cd5c8738a02ac3d90d105b0
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-26 20:18:08 +0700

    any

--- src/learn.ts
+++ src/learn.ts
@@ -1,16 +1,4 @@
-enum MatchResult { Win, Draw, Lose }
+let someValue: any = 'this is a string';
 
-function getPoint(result: MatchResult) {
-    if (result === MatchResult.Win) return 3;
-    if (result === MatchResult.Draw) return 1;
-    return 0;
-}
-
-console.log(getPoint(MatchResult.Win));
-console.log(getPoint(0));
-
-console.log(getPoint(MatchResult.Draw));
-console.log(getPoint(1));
-
-console.log(getPoint(MatchResult.Lose));
-console.log(getPoint(2));
+console.log((someValue as string).length);
+console.log((<string>someValue).length);
